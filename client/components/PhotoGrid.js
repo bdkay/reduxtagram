e0b71ee6f25954a804e43983aca1fb7f731a4114
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-16 14:37:35 -0500

    Skeleton components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PhotoGrid extends Component {
+  render(){
+    return (
+      <div class>
+        PhotoGrid
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid;
+
