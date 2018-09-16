2302fc1aca65a97b0d8f8eca6fd6ca35e2283598
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-16 15:34:43 -0500

    Push state

--- client/index.js
+++ client/index.js
@@ -4,18 +4,17 @@ import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 import css from './styles/style.styl';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, Redirect } from 'react-router';
 
 const router = (
-  <Router
-  history={browserHistory}
-  >
+  <Router history={browserHistory}>
     <Route path="/" component={Main}>
-      <IndexRoute component="{PhotoGrid}"></IndexRoute>
-      <Route path="view/:postId" component="{Single}"></Route>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="view/:postId" component={Single}></Route>
     </Route>
+    <Route path="*" component={Main} />
   </Router>
 );
 
 
-render(<Main />, document.getElementById('root'));
+render(router, document.getElementById('root'));
