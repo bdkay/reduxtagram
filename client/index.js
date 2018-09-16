8ccaf8d0aa94dfa050be7cf8ac3c6190c35322b0
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-16 14:16:34 -0500

    Reduxtagram 1.0

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './components/Main';
+
+// Import CSS
+import css from './styles/style.styl';
+
+
+render(<Main />, document.getElementById('root'));
