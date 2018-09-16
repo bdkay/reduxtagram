8b8fdcd3ea1357fd61a470791081751f5135079b
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-16 17:33:31 -0500

    Some html

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -5,7 +5,7 @@ class PhotoGrid extends Component {
   render(){
     return (
       <div className='photo-grid'>
-        { this.props.posts.map((post, index) => <Photo />) }
+        { this.props.posts.map((post, index) => <Photo {...this.props} key={index} post={post} />) }
       </div>
     )
   }
