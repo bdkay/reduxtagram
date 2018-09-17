37df27e2b933ebd5c50163caec58ef486052d7ed
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-09-16 20:23:21 -0500

    I'm a comment

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
 
 class Single extends Component {
   render(){
+    
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+  
+    const postComments = this.props.comments[this.props.params.postId] || [];
+  
     return (
-      <div className='single-photo'>
-       I'm the single photo
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     )
   }
